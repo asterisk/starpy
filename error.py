685bdd14f2098ef7b283c421f50e1cb17cf54ded
Paul Belanger <pabelanger@digium.com>
2011-04-18 17:51:37 +0000

    Formatting changes; :retab to remove red blobs
    git-svn-id: https://starpy.svn.sourceforge.net/svnroot/starpy/trunk@45 b6236cd2-a710-0410-8d3e-adb8ec5871e9

--- error.py
+++ error.py
@@ -1,18 +1,17 @@
 """Collection of StarPy-specific error classes"""
 
 class AMICommandFailure( Exception ):
-	"""AMI Command failure of some description"""
+    """AMI Command failure of some description"""
 class AGICommandFailure( Exception ):
-	"""AGI Command failure of some description"""
+    """AGI Command failure of some description"""
 
 class MenuFinished( Exception ):
-	"""Base class for reporting non-standard exits (i.e. not a choice) from a menu"""
+    """Base class for reporting non-standard exits (i.e. not a choice) from a menu"""
 
 class MenuExit( MenuFinished ):
-	"""User exited from the menu voluntarily"""
+    """User exited from the menu voluntarily"""
 
 class MenuTimeout( MenuFinished ):
-	"""User didn't complete selection from menu in reasonable time period"""
+    """User didn't complete selection from menu in reasonable time period"""
 class MenuUnexpectedOption( MenuFinished ):
-	"""Somehow the user managed to select an option that doesn't exist?"""
-	
+    """Somehow the user managed to select an option that doesn't exist?"""
