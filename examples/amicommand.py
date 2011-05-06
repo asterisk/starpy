c6d86dbfbb7417efce0805b02e9684cb1c37deeb
pabelanger <paul.belanger@polybeacon.com>
2011-05-06 19:49:14 -0400

    Formatting changes; :retab to remove red blobs.
    Signed-off-by: pabelanger <paul.belanger@polybeacon.com>

--- examples/amicommand.py
+++ examples/amicommand.py
@@ -10,25 +10,24 @@ log = logging.getLogger( 'callduration' )
 APPLICATION = utilapplication.UtilApplication()
 
 def main():
-	def onConnect( ami ):
-		def onResult( result ):
-			print 'Result', result 
-			return ami.logoff()
-		def onError( reason ):
-			print reason.getTraceback()
-			return reason
-		def onFinished( result ):
-			reactor.stop()
-		df = ami.command( 'database show' )
-		df.addCallbacks( onResult, onError )
-		df.addCallbacks( onFinished, onFinished )
-		return df
-	amiDF = APPLICATION.amiSpecifier.login( 
-	).addCallback( onConnect )
+    def onConnect( ami ):
+        def onResult( result ):
+            print 'Result', result
+            return ami.logoff()
+        def onError( reason ):
+            print reason.getTraceback()
+            return reason
+        def onFinished( result ):
+            reactor.stop()
+        df = ami.command( 'database show' )
+        df.addCallbacks( onResult, onError )
+        df.addCallbacks( onFinished, onFinished )
+        return df
+    amiDF = APPLICATION.amiSpecifier.login(
+    ).addCallback( onConnect )
 
 if __name__ == "__main__":
-	logging.basicConfig()
-	manager.log.setLevel( logging.DEBUG )
-	reactor.callWhenRunning( main )
-	reactor.run()
-
+    logging.basicConfig()
+    manager.log.setLevel( logging.DEBUG )
+    reactor.callWhenRunning( main )
+    reactor.run()
