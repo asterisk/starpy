56ae6b982e0086ca3c0d7e37265122094476a91c
Paul Belanger <paul.belanger@polybeacon.com>
2012-02-15 09:53:51 -0500

    Prepare for 1.0.1 release
    Signed-off-by: Paul Belanger <paul.belanger@polybeacon.com>

--- setup.py
+++ setup.py
@@ -85,7 +85,7 @@ from a Twisted process.""",
     ### Now the actual set up call
     setup (
         name = "starpy",
-        version = '1.0.0',
+        version = '1.0.1',
         url = "http://asterisk-org.github.com/starpy/",
         description = "Twisted Protocols for interaction with the Asterisk PBX",
         author = "Mike C. Fletcher",
